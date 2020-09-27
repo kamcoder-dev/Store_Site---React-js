9cb566e4c0568ff7dc9381f4a8f7f1d582205053
kamran <kamcoder94@gmail.com>
2020-09-27 18:25:46 +0100

    first commit

---
+++ src/data/placeholderData.js
@@ -0,0 +1,10 @@
+export const data = {
+    categories: ["Watersports", "Soccor", "Chess", "Running"],
+    products: [
+        { id: 1, name: "P1", catogory: "Watersports", description: "P1 (Watersports)", price: 3 },
+        { id: 2, name: "P2", catogory: "Watersports", description: "P2 (Watersports)", price: 4 },
+        { id: 3, name: "P3", catogory: "Running", description: "P3 (Running)", price: 5 },
+        { id: 4, name: "P4", catogory: "Chess", description: "P4 (Chess)", price: 6 },
+        { id: 5, name: "P5", catogory: "Chess", description: "P5 (Chess)", price: 7 },
+    ]
+}
